8f9a0874131962b6d3aabbda4ff8221501203a26
whiskas-cmd <sham1991.ss@gmail.com>
2026-06-19 11:55:06 +0300

    Create 3-4Home.py

---
+++ 3-4Home.py
@@ -0,0 +1,36 @@
+a = 5
+b = 7
+print(f'{a + b * a + b * a + b}={a * a * a + 3 * a * a * b + 3 * a * b * b + b * b * b * b}')
+print(f'{a - b * a - b * a - b}={a * a * a - 3 * a * a * b + 3 * a * b * b - b * b * b * b}')
+print(f'{a * a * a + b * b * b}={a + b}*{a * a - a * b + b * b}')
+print(f'{a * a * a - b * b * b}={a - b}*{a * a + a * b + b * b}')
+
+#######
+a = 12
+b = 3.5
+c = 2
+
+print(int(a))
+x=12
+print(float(b))
+y=3.5
+print(f'{x}+{y}+{c}={x+y+c}')
+result=17.5
+print(type(result))
+
+#########
+
+a=2
+b=5
+
+print(f'{a}+{b}={a+b}')
+print(f'{a}-{b}={a-b}')
+print(f'{a}*{b}={a*b}')
+print(f'{a}//{b}={a//b}')
+
+########
+
+name='Gregory'
+age=20
+print(f'привіт,{name} {name} {name}')
+print(f'Тобі {age} років')
